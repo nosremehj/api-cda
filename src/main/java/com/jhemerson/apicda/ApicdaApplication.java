7dd31b8fb090d44c2e5603696787be3096b639bc
tijhemerson <jhemerson1931@gmail.com>
2022-09-01 22:10:10 -0300

    envio inicial da API

---
+++ src/main/java/com/jhemerson/apicda/ApicdaApplication.java
@@ -0,0 +1,13 @@
+package com.jhemerson.apicda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhemerson/apicda/ApicdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhemerson.apicda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
